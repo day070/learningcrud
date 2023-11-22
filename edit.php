5a04f73d20edeca2d31f3f18e153e3332c320aad
day070 <143053861+day070@users.noreply.github.com>
2023-11-23 00:41:20 +0800

    Update edit.php
    memperbaiki bang

--- edit.php
+++ edit.php
@@ -1,14 +1,12 @@
 <?php
+$id = $_GET['ID'];
 
 include_once("koneksi.php");
 
-if(isset($_POST['update'])){
-    $nisn = $_POST['nisn'];
+if (isset($_POST['update'])) {
     $nama = $_POST['nama'];
-    $cita = $_POST['cita_cita'];
-    $jnskelamin = $_POST['jnskelamin'];
-
-    $result = mysqli_query($mysqli,"UPDATE SISWA SET NAMA='$nama',CITA_CITA='$cita',JNSKELAMIN='$jnskelamin' WHERE NISN=$nisn");
+    $harga = $_POST['harga'];
+    $result = mysqli_query($koneksi, "UPDATE motor SET NAMA='$nama',HARGA='$harga' WHERE ID=$id");
 
     header("location: index.php");
 }
@@ -16,46 +14,42 @@ if(isset($_POST['update'])){
 ?>
 
 <?php
-$nisn = $_GET['nisn'];
 
-$result = mysqli_query($mysqli, "SELECT * FROM SISWA WHERE NISN=$nisn");
+$result = mysqli_query($koneksi, "SELECT * FROM motor WHERE ID=$id");
 
-while($data_siswa = mysqli_fetch_array($result))
-{
-    $nama = $data_siswa['NAMA'];
-    $cita = $data_siswa['CITA_CITA'];
-    $jnskelamin = $data_siswa['JNSKELAMIN'];
+while ($data_siswa = mysqli_fetch_array($result)) {
+    $Nama = $data_siswa['NAMA'];
+    $Harga = $data_siswa['HARGA'];
 }
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>UPDATE</title>
 </head>
+
 <body>
     <a href="index.php">Balik</a><br><br>
-    <form action="edit.php" name="edit_siswa" method="post">
+    <form action='' method="post">
         <table>
             <tr>
                 <td>Nama</td>
-                <td><input type="text" name="nama" value="<?php echo $nama; ?>"></td>
+                <td><input type="text" name="nama" value="<?php echo $Nama; ?>"></td>
             </tr>
             <tr>
-                <td>Cita-cita</td>
-                <td><input type="text" name="cita_cita" value="<?php echo $cita; ?>"></td>
+                <td>Harga</td>
+                <td><input type="text" name="harga" value="<?php echo $Harga; ?>"></td>
             </tr>
             <tr>
-                <td>Jenis kelamin</td>
-                <td><input type="text" name="jnskelamin" value="<?php echo $jnskelamin; ?>"></td>
-            </tr>
-            <tr>
-                <td><input type="hidden" name="nisn" value="<?php echo $_GET['nisn']; ?>"></td>
+                <td><input type="hidden" name="id" value="<?php echo $_GET['ID']; ?>"></td>
                 <td><input type="submit" name="update" value="UPDATE"></td>
             </tr>
         </table>
     </form>
 </body>
+
 </html>
